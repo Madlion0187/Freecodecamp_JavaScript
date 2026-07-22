aba790ec7dbe5e6b6a89047ec61ccc1de450ed4a
Madlion0187 <norbi0187@gmail.com>
2026-07-22 20:09:55 +0200

    Logic checker app

---
+++ 02_Booleans_and_Numbers/01_logic_checker_app/main.js
@@ -0,0 +1,32 @@
+const truthyOrFalsy = true;
+console.log(Boolean(truthyOrFalsy));
+
+const truthyOrFalsy02 = "freeCodeCamp";
+console.log(Boolean(truthyOrFalsy02));
+
+const truthyOrFalsy03 = "";
+console.log(Boolean(truthyOrFalsy03));
+
+const hasDeveloperJob = true;
+if (hasDeveloperJob) {
+    console.log("Timmy is employed as a developer.");
+}
+
+const isTimmyAGamer = false;
+if (isTimmyAGamer) {
+    console.log("Timmy loves to play World of Warcraft.");
+}
+
+const timmyAge = 18;
+if (timmyAge >= 16) {
+    console.log("Timmy is old enough to drive.");
+} else {
+    console.log("Timmy is not old enough to drive.");
+}
+
+const timmyAge2 = 15;
+if (timmyAge2 >= 16) {
+    console.log("Timmy is old enough to drive.");
+} else {
+    console.log("Timmy is not old enough to drive.");
+}
