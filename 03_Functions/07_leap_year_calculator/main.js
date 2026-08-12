cc64e67136f38c34d186a0b8e701e0b2d6c64f05
Madlion0187 <norbi0187@gmail.com>
2026-08-12 20:56:27 +0200

    Leap year calculator

---
+++ 03_Functions/07_leap_year_calculator/main.js
@@ -0,0 +1,13 @@
+let year = 1900;
+
+function isLeapYear(year) {
+    if((year % 400 === 0) || ((year % 4 === 0) && (year % 100 !== 0))) {
+        return `${year} is a leap year.`;
+    }else {
+        return `${year} is not a leap year.`;
+    }
+}
+
+const result = isLeapYear(year);
+
+console.log(result);
