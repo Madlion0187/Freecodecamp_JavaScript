dd94542ba5a53fd5efa2c24e64a80a8326625093
Madlion0187 <norbi0187@gmail.com>
2026-08-29 16:10:42 +0200

    Lunch picker program

---
+++ 04_Arrays/02_lunch_picker_program/main.js
@@ -0,0 +1,77 @@
+let lunches = [];
+
+// Add a lunch to the end
+function addLunchToEnd(arr, luncItem) {
+    arr.push(luncItem);
+    console.log(`${luncItem} added to the end of the lunch menu.`);
+    return arr;
+}
+
+//console.log(addLunchToEnd(lunches, "Taco"));
+//console.log(addLunchToEnd(["Pizza", "Tacos"], "Burger"));
+
+// Add a lunch to the start
+function addLunchToStart(arr, luncItem) {
+    arr.unshift(luncItem);
+    console.log(`${luncItem} added to the start of the lunch menu.`);
+    return arr;
+}
+
+//console.log(addLunchToStart(lunches, "Sushi"));
+//console.log(addLunchToStart(["Burger", "Sushi"], "Pizza"));
+
+// Remove a lunch from the end
+function removeLastLunch(arr) {
+    if(arr.length > 0) {
+        let lastLunch = arr.pop();
+        console.log(`${lastLunch} removed from the end of the lunch menu.`);
+    }else {
+        console.log("No lunches to remove.");
+    }
+    return arr;
+}
+
+//console.log(removeLastLunch(lunches));
+//console.log(removeLastLunch(["Stew", "Soup", "Toast"]));
+//console.log(removeLastLunch(["Sushi", "Pizza", "Noodles"]));
+
+// Remove a lunch from the start
+function removeFirstLunch(arr) {
+    if(arr.length > 0) {
+        let firstLunch = arr.shift();
+        console.log(`${firstLunch} removed from the start of the lunch menu.`);
+    }else {
+        console.log("No lunches to remove.");
+    }
+    return arr;
+}
+
+//console.log(removeFirstLunch(lunches));
+//console.log(removeFirstLunch(["Salad", "Eggs", "Cheese"]));
+//console.log(removeFirstLunch(["Sushi", "Pizza", "Burger"]));
+
+// Select random lunch
+function getRandomLunch(arr) {
+    let randomLunch = arr[(Math.floor(Math.random() * arr.length))];
+    if(arr.length > 0) {
+        return `Randomly selected lunch: ${randomLunch}`;
+    }else {
+        return "No lunches available.";
+    }
+}
+
+//console.log(getRandomLunch(lunches));
+//console.log(getRandomLunch(["Sushi", "Pizza", "Burger"]));
+
+// Show lunch menu
+function showLunchMenu(arr) {
+    if(arr.length > 0) {
+        return `Menu items: ${arr}`;
+    }else {
+        return "The menu is empty.";
+    }
+}
+
+console.log(showLunchMenu(lunches));
+console.log(showLunchMenu(["Greens", "Corns", "Beans"]));
+console.log(showLunchMenu(["Pizza", "Burger", "Fries", "Salad"]));
